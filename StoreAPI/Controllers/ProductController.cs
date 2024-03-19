ed52a37fab1f34ca33934ecc68c2c9fefedae20b
wiroon.sri@C-709 <wiroon.sri@innosoft.kmutt.ac.th>
2024-03-19 22:55:54 +0700

    api for test db connection

---
+++ StoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using StoreAPI.Data;
+using StoreAPI.Models;
+
+namespace StoreAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly ApplicationDbContext context;
+
+    public ProductController(ApplicationDbContext _context)
+    {
+        context = _context;
+    }
+
+    // Test db connection
+    // GET: /api/Product/testconnectdb
+    [HttpGet("testconnectdb")]
+    public void TestConnection()
+    {
+        if (context.Database.CanConnect())
+        {
+            Response.WriteAsync("Connected");
+        }
+        else
+        {
+            Response.WriteAsync("Not Connected");
+        }
+    }
+}
