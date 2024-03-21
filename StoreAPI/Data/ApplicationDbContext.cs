5a5445990b1fac23c21ed0c8513831bf3bd5ce7f
wiroon.sri@WSYMBP16M2 <wiroon.sri@innosoft.kmutt.ac.th>
2024-03-21 13:52:00 +0700

    update app db context

--- StoreAPI/Data/ApplicationDbContext.cs
+++ StoreAPI/Data/ApplicationDbContext.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using StoreAPI.Models;
 
 namespace StoreAPI.Data;
-
-public partial class ApplicationDbContext : DbContext
+public partial class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -18,6 +17,9 @@ public partial class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // เพิ่มส่วนนี้เข้าไป
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<category>(entity =>
         {
             entity.HasKey(e => e.category_id).HasName("categories_pkey");
